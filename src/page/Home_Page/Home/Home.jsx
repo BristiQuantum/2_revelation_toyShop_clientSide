d0d537ef070a13a681afbe0127838662c633e06d
BristiQuantum <bristi.get.contact@gmail.com>
2024-05-22 15:47:16 +0600

    change logo and set a error for home page

--- src/page/Home_Page/Home/Home.jsx
+++ src/page/Home_Page/Home/Home.jsx
@@ -1,8 +1,8 @@
-
+import img from '../../../assets/home/t.png'
 
 const Home = () => {
     return (
-        <div className="home">
+        <div className="home" style={{ backgroundImage: `url(${img})`, backgroundPosition: 'center' }}>
             <div className="sm:pl-48 space-y-5">
                 <h1 className='h1'>Get a new friend !</h1>
                 <button className='btn-fill'>Shop Now</button>
